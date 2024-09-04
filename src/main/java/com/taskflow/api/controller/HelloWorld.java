b7b708d08ff81ae376d74f55d3f8d12119baec5a
chwangsky <siztheday25@gmail.com>
2024-09-04 16:30:35 +0900

    init commit

---
+++ src/main/java/com/taskflow/api/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.taskflow.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("HELLO!");
+
+        return "HELLO!";
+    }
+
+}
